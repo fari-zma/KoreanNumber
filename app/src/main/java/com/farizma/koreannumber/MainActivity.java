fe79e88ee661adab98b35d0beffc23760322fbd7
fari-zma <ayaanthesuperstar@gmail.com>
2020-05-12 19:25:40 +0530

    Update

--- app/src/main/java/com/farizma/koreannumber/MainActivity.java
+++ app/src/main/java/com/farizma/koreannumber/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.one);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
@@ -58,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onDestroy() {
+        super.onDestroy();
         mediaPlayer.release();
     }
 }
